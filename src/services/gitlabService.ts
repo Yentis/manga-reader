94ce6d9ea2b2cfb1a8044381f46af429980e52bd
Yentis <yentl178@gmail.com>
2021-09-10 22:15:47 +0200

    replace lightning-fs with browserfs

--- src/services/gitlabService.ts
+++ src/services/gitlabService.ts
@@ -5,21 +5,29 @@ import { NotifyOptions } from 'src/classes/notifyOptions'
 import { UrlNavigation } from '../classes/urlNavigation'
 import constants from 'src/classes/constants'
 import git from 'isomorphic-git'
-import LightningFS, { FSConstructorOptions } from '@isomorphic-git/lightning-fs'
 import http from 'isomorphic-git/http/web'
+import { configure, BFSRequire } from 'browserfs/dist/node/index'
+import { FSModule } from 'browserfs/dist/node/core/FS'
 
 const CLIENT_ID = '1ac7147c66b40b6aaae3f3fd0cac5169d26fd4b406e6198f4b3fd1fd29d9816a'
 const GITLAB_DIR = '/gitlab'
 const SNIPPET_FILE_NAME = 'manga_list.json'
 
-const fs = new LightningFS('fs', { wipe: true } as FSConstructorOptions)
+configure({
+  fs: 'InMemory',
+  options: {}
+}, (error) => {
+  if (error) console.error(error)
+})
+
+const fs = BFSRequire('fs')
 let initialized = false
 
 async function initGit (shareId: string) {
   if (initialized) return
   initialized = true
 
-  await fs.promises.mkdir(GITLAB_DIR)
+  await mkdir(GITLAB_DIR)
   await git.clone({
     fs,
     http,
@@ -31,7 +39,20 @@ async function initGit (shareId: string) {
   })
 }
 
-async function resetToOldestCommit (fs: LightningFS) {
+function mkdir (path: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path, '0777', (error) => {
+      if (error) {
+        reject(error)
+        return
+      }
+
+      resolve()
+    })
+  })
+}
+
+async function resetToOldestCommit (fs: FSModule) {
   const dir = GITLAB_DIR
   const commits = await git.log({ fs, dir })
   if (commits.length <= 1) return
@@ -41,9 +62,22 @@ async function resetToOldestCommit (fs: LightningFS) {
   const branch = await git.currentBranch({ fs, dir })
   if (typeof branch !== 'string') return
 
-  await fs.promises.writeFile(`${dir}/.git/refs/heads/${branch}`, data)
+  await writeFile(`${dir}/.git/refs/heads/${branch}`, Buffer.from(data))
   await git.resetIndex({ fs, dir, filepath: SNIPPET_FILE_NAME })
   await git.checkout({ fs, dir })
+}
+
+function writeFile (path: string, data: unknown): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (error) => {
+      if (error) {
+        reject(error)
+        return
+      }
+
+      resolve()
+    })
+  })
 }
 
 export function getShareId (): string {
@@ -99,7 +133,7 @@ export async function updateList (list: string): Promise<void> {
 
   const dir = GITLAB_DIR
   const data = new TextEncoder().encode(getModifiedList(list))
-  await fs.promises.writeFile(`${dir}/${SNIPPET_FILE_NAME}`, data)
+  await writeFile(`${dir}/${SNIPPET_FILE_NAME}`, Buffer.from(data))
   await git.add({ fs, dir, filepath: SNIPPET_FILE_NAME })
 
   await git.commit({
